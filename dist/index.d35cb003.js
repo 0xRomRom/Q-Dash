3c52d4204a8f0386515de6c7cf9abafbcc8d845c
0xromrom <roman0811@live.nl>
2022-09-26 06:17:41 -0700

    Added animation on succesful fetch

--- dist/index.d35cb003.js
+++ dist/index.d35cb003.js
@@ -541,6 +541,8 @@ const searchCoinButton = document.querySelector(".seach-coin-btn");
 const addressInput = document.querySelector(".contract-input");
 const blockchains = document.querySelector(".blockchains");
 const errorText = document.querySelector(".error-text");
+const resultContainer = document.querySelector(".result-container");
+const inputContainer = document.querySelector(".input-container");
 closeContractSearch.addEventListener("click", ()=>{
     searchContractBox.classList.add("hidden");
     dropShadow.classList.add("hidden");
@@ -553,11 +555,19 @@ searchCoinButton.addEventListener("click", ()=>{
     errorText.classList.add("inv");
     coinFetcher();
 });
+// 7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU
+const resultModal = (data)=>{};
 const coinFetcher = async ()=>{
     try {
         const response = await fetch(`https://api.coingecko.com/api/v3/coins/${blockchains.value}/contract/${addressInput.value}`);
         const data = await response.json();
-        if (data.error) errorText.classList.remove("inv");
+        if (!response.ok) {
+            errorText.classList.remove("inv");
+            return;
+        }
+        inputContainer.classList.add("slideOutDiv");
+        resultContainer.classList.remove("hidden");
+        resultContainer.classList.add("slideInDiv");
     } catch (err) {
         console.log(err);
     }
--- scripts/searchByContract.js
+++ scripts/searchByContract.js
@@ -8,6 +8,8 @@ const searchCoinButton = document.querySelector(".seach-coin-btn");
 const addressInput = document.querySelector(".contract-input");
 const blockchains = document.querySelector(".blockchains");
 const errorText = document.querySelector(".error-text");
+const resultContainer = document.querySelector(".result-container");
+const inputContainer = document.querySelector(".input-container");
 
 closeContractSearch.addEventListener("click", () => {
   searchContractBox.classList.add("hidden");
@@ -24,15 +26,23 @@ searchCoinButton.addEventListener("click", () => {
   coinFetcher();
 });
 
+// 7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU
+
+const resultModal = (data) => {};
+
 const coinFetcher = async () => {
   try {
     const response = await fetch(
       `https://api.coingecko.com/api/v3/coins/${blockchains.value}/contract/${addressInput.value}`
     );
     const data = await response.json();
-    if (data.error) {
+    if (!response.ok) {
       errorText.classList.remove("inv");
+      return;
     }
+    inputContainer.classList.add("slideOutDiv");
+    resultContainer.classList.remove("hidden");
+    resultContainer.classList.add("slideInDiv");
   } catch (err) {
     console.log(err);
   }
