91fd4126b386c8077b8c870af3b17bc9fe2cf7bd
0xromrom <roman0811@live.nl>
2022-09-26 18:20:33 -0700

    Started working on the notifications modal

--- dist/index.ec980dba.js
+++ dist/index.ec980dba.js
@@ -537,6 +537,11 @@ const manageNotification = document.querySelector(".man-notif");
 const notificationModal = document.querySelector(".notification-box");
 const dropShadow = document.querySelector(".dropshadow");
 const closeNotifModal = document.querySelector(".light-close3");
+const notifPush = document.querySelector(".notif-push");
+const notifSearch = document.querySelector(".notif-search");
+const coinFetchPrice = document.querySelector(".coin-fetch-output");
+const userInput = document.querySelector(".user-notif-coin");
+let permission = false;
 manageNotification.addEventListener("click", ()=>{
     notificationModal.classList.remove("hidden");
     dropShadow.classList.remove("hidden");
@@ -545,6 +550,29 @@ closeNotifModal.addEventListener("click", ()=>{
     notificationModal.classList.add("hidden");
     dropShadow.classList.add("hidden");
 });
+dropShadow.addEventListener("click", ()=>{
+    notificationModal.classList.add("hidden");
+    dropShadow.classList.add("hidden");
+});
+notifPush.addEventListener("click", ()=>{
+    Notification.requestPermission().then((perm)=>{
+        if (perm === "granted") permission = true;
+    });
+});
+notifSearch.addEventListener("click", ()=>{
+    fetchCoinAlert(userInput.value);
+    if (permission) {
+        const notification = new Notification("Example Notification", {
+            body: "This is more text"
+        });
+    }
+});
+const fetchCoinAlert = async ()=>{
+    const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${userInput.value}&vs_currencies=usd`);
+    const data = await response.json();
+    console.log(data[userInput.value].usd);
+    coinFetchPrice.textContent = `$${data[userInput.value].usd}`;
+};
 
 },{}]},["a37Ji","4yZjd"], "4yZjd", "parcelRequire379f")
 
--- scripts/manageNotifications.js
+++ scripts/manageNotifications.js
@@ -4,6 +4,12 @@ const manageNotification = document.querySelector(".man-notif");
 const notificationModal = document.querySelector(".notification-box");
 const dropShadow = document.querySelector(".dropshadow");
 const closeNotifModal = document.querySelector(".light-close3");
+const notifPush = document.querySelector(".notif-push");
+const notifSearch = document.querySelector(".notif-search");
+const coinFetchPrice = document.querySelector(".coin-fetch-output");
+const userInput = document.querySelector(".user-notif-coin");
+
+let permission = false;
 
 manageNotification.addEventListener("click", () => {
   notificationModal.classList.remove("hidden");
@@ -14,3 +20,34 @@ closeNotifModal.addEventListener("click", () => {
   notificationModal.classList.add("hidden");
   dropShadow.classList.add("hidden");
 });
+
+dropShadow.addEventListener("click", () => {
+  notificationModal.classList.add("hidden");
+  dropShadow.classList.add("hidden");
+});
+
+notifPush.addEventListener("click", () => {
+  Notification.requestPermission().then((perm) => {
+    if (perm === "granted") {
+      permission = true;
+    }
+  });
+});
+
+notifSearch.addEventListener("click", () => {
+  fetchCoinAlert(userInput.value);
+  if (permission) {
+    const notification = new Notification("Example Notification", {
+      body: "This is more text",
+    });
+  }
+});
+
+const fetchCoinAlert = async () => {
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/simple/price?ids=${userInput.value}&vs_currencies=usd`
+  );
+  const data = await response.json();
+  console.log(data[userInput.value].usd);
+  coinFetchPrice.textContent = `$${data[userInput.value].usd}`;
+};
