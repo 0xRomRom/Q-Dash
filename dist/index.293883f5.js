5279c140d1ad02a16430eb284cc8a13637005c64
0xromrom <roman0811@live.nl>
2022-10-03 18:02:11 -0700

    More styling

--- dist/index.293883f5.js
+++ dist/index.293883f5.js
@@ -602,7 +602,7 @@ const nameChecker = (data)=>{
     }
 };
 // Re-fetching coin and displaying result
-const dataFetcher = async (id = "samoyedcoin")=>{
+const dataFetcher = async (id = "bitcoin")=>{
     try {
         const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}&order=market_cap_desc&per_page=100&page=1&sparkline=false`);
         const data = await response.json();
