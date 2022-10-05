de246113ae2a164b3c630a8beebd973c229e269d
0xromrom <roman0811@live.nl>
2022-10-05 11:50:40 -0700

    Search funtionality rounded up

--- scripts/searchCoin.js
+++ scripts/searchCoin.js
@@ -3,11 +3,16 @@
 const searchModalButton = document.querySelector(".search-button");
 const searchModal = document.querySelector(".search-modal");
 const searchModalInner = document.querySelector(".search-inner-modal");
+const searchModalResult = document.querySelector(".search-inner-result");
+const searchModalTitle = document.querySelector(".search-modal-title");
 const dropBg = document.querySelector(".dropshadow-srch");
 const closeSearchModal = document.querySelector(".light-close4");
 const searchInput = document.querySelector(".search-coin-input");
 const searchButton = document.querySelector(".search-coin-btn");
 const cantFindText = document.querySelector(".cant-find");
+const buttonBox = document.querySelector(".cntr-sr-btn");
+const searchBackButton = document.querySelector(".search-back");
+const resultBoxes = document.querySelectorAll(".out");
 
 const responseImage = document.querySelector(".res-img");
 const upDown = document.querySelector(".up-down");
@@ -27,7 +32,6 @@ const circulatingPercentageBox = document.querySelector(".circ-perc-box");
 const circulatingCountBox = document.querySelector(".circ-amount-box");
 
 // Date array
-
 const monthsArray = [
   "January",
   "February",
@@ -43,11 +47,56 @@ const monthsArray = [
   "December",
 ];
 
+// Toggle display of circulating supply
 circulatingPercentageBox.addEventListener("mouseover", () => {
   circulatingCountBox.classList.remove("inv");
 });
 circulatingPercentageBox.addEventListener("mouseleave", () => {
   circulatingCountBox.classList.add("inv");
+});
+
+// Back from search result
+searchBackButton.addEventListener("click", () => {
+  searchModalInner.classList.remove("rotateDiv");
+  searchModalResult.classList.remove("fadeDivIn");
+  searchModalResult.classList.add("rotateDivBack");
+  searchModalTitle.classList.add("fadeOut");
+  searchInput.classList.add("fadeOut");
+  buttonBox.classList.add("fadeOut");
+  searchInput.value = "";
+  searchBackButton.classList.add("hidden");
+  resultBoxes.forEach((item) => {
+    item.classList.add("fadeOut");
+  });
+  setTimeout(() => {
+    searchModalTitle.classList.remove("fadeOut");
+    searchInput.classList.remove("fadeOut");
+    buttonBox.classList.remove("fadeOut");
+    searchModalTitle.classList.add("fadeDivIn");
+    searchInput.classList.add("fadeDivIn");
+    buttonBox.classList.add("fadeDivIn");
+    searchModalResult.classList.remove("rotateDivBack");
+    searchModalResult.classList.add("hidden");
+
+    fluctuate.textContent = "";
+    SearchCoinTitle.textContent = "";
+    fetchedRank.textContent = "";
+    circulatingPercentage.textContent = "";
+    pricePercentageChange.textContent = "";
+    totalSupply.textContent = "";
+    markPrice.textContent = "";
+    markPriceHigh.textContent = "";
+    markPriceLow.textContent = "";
+    athDate.textContent = "";
+    circulatingCount.textContent = "";
+
+    searchInput.focus();
+    resultBoxes.forEach((item) => {
+      item.classList.remove("fadeOut");
+      item.classList.remove("bg-up");
+      item.classList.remove("bg-down");
+    });
+  }, 400);
 });
 
 //Open search modal
@@ -58,6 +107,7 @@ searchModalButton.addEventListener("click", () => {
   searchModal.classList.remove("hidden");
   searchModal.classList.remove("fadeOut");
   searchModal.classList.remove("hidden");
+  searchInput.focus();
 });
 
 //Close search modal
@@ -104,10 +154,19 @@ const coinSearcher = async () => {
     const data = await response.json();
     if (data.coins.length === 0) {
       cantFindText.classList.remove("inv");
+      return;
     } else {
       nameChecker(data);
       searchModalInner.classList.add("rotateDiv");
+      searchBackButton.classList.remove("hidden");
       cantFindText.classList.add("inv");
+      searchModalTitle.classList.add("fadeOut");
+      searchInput.classList.add("fadeOut");
+      buttonBox.classList.add("fadeOut");
+      searchModalResult.classList.add("fadeDivIn");
+      setTimeout(() => {
+        searchModalResult.classList.remove("hidden");
+      }, 500);
     }
   } catch (err) {
     console.log(err);
@@ -128,7 +187,7 @@ const nameChecker = (data) => {
 };
 
 // Re-fetching coin and displaying result
-const dataFetcher = async (id = "ethereum-name-service") => {
+const dataFetcher = async (id) => {
   try {
     const response = await fetch(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}&order=market_cap_desc&per_page=100&page=1&sparkline=false`
@@ -139,14 +198,11 @@ const dataFetcher = async (id = "ethereum-name-service") => {
     );
     const data2 = await response2.json();
     const resultPercentage = data2[id].usd_24h_change.toString().slice(0, 4);
-    console.log(data[0]);
-    console.log(data2[id]);
     displayUI(data[0], resultPercentage);
   } catch (err) {
     console.log(err);
   }
 };
-dataFetcher();
 
 const dateConverter = (month) => {
   let slicedMonth = +month.slice(5, 7);
@@ -154,31 +210,36 @@ const dateConverter = (month) => {
 };
 
 const displayUI = (data, percentage) => {
-  console.log(data.current_price.toString().length);
-
   let date = data.ath_date.slice(0, 10);
+  console.log(data.price_change_24h);
+  let stringFloat = 6;
   circulatingPercentage.textContent =
-    data.circulating_supply / data.max_supply === Infinity
+    data.circulating_supply / data.total_supply === Infinity
       ? "∞"
-      : `${(data.circulating_supply / data.max_supply)
+      : `${(data.circulating_supply / data.total_supply)
           .toString()
           .slice(2, 4)}%`;
 
   responseImage.src = data.image;
   SearchCoinTitle.textContent = data.name;
   fetchedRank.textContent = `#${data.market_cap_rank}`;
+
   pricePercentageChange.textContent = `${
-    percentage >= 0 ? "+" : ""
+    percentage >= 0.01 ? "+" : ""
   }${percentage}% $${data.symbol.toUpperCase()} ${
-    "$" + percentage >= 0 ? "+" : ""
+    "$" + percentage >= 0.01 ? "+" : ""
   }${
     data.current_price.toString().length >= 8
       ? ""
-      : "(" + data.price_change_24h.toString().slice(0, 7) + ")"
+      : "(" +
+        data.price_change_24h.toString().slice(0, stringFloat) +
+        " USD" +
+        ")"
   }`;
   totalSupply.textContent =
-    data.total_supply === null ? "∞" : data.total_supply.toFixed(0);
-
+    data.total_supply === null
+      ? "∞"
+      : data.total_supply.toFixed(0) + " " + data.symbol.toUpperCase();
   markPrice.textContent = `$${data.current_price} USD`;
   markPriceHigh.textContent = `$${
     data.current_price > 1 ? data.high_24h.toFixed(2) : data.high_24h.toFixed(5)
