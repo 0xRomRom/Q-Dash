1baa8a912e721fd14b06dd2d6f61158b5cc83f50
0xromrom <roman0811@live.nl>
2022-10-13 12:03:50 -0700

    Search functionality working

--- dist/index.9db9efab.js
+++ dist/index.9db9efab.js
@@ -533,6 +533,16 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"3X4BY":[function(require,module,exports) {
 "use strict";
+// const sendData = fetch(
+//   `https://qdash-3fe95-default-rtdb.europe-west1.firebasedatabase.app/${getStorage}.json`,
+//   {
+//     method: "POST",
+//     body: JSON.stringify(someInput.value),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   }
+// );
 const watchListButton = document.querySelector(".watchlist");
 const watchListClose = document.querySelector(".watch-close");
 const watchListDiv = document.querySelector(".watchlist-div");
@@ -547,6 +557,7 @@ const closeSearchModal = document.querySelector(".inner-search-close");
 const addItemDiv = document.querySelector(".add-item-div");
 const searchInputButton = document.querySelector(".add-search-button");
 const searchCoinInput = document.querySelector(".coin-search-input");
+const noCoinFoundTxt = document.querySelector(".no-search-res");
 // Toggle to watchlist view
 viewWatchlist.addEventListener("click", ()=>{
     viewWatchlist.classList.remove("fadeColorOut");
@@ -554,10 +565,9 @@ viewWatchlist.addEventListener("click", ()=>{
     viewAssets.classList.add("fadeColorOut");
     watchListBox.classList.remove("hidden");
     watchListBox.classList.add("fadeBoxIn");
-    assetsBox.classList.add("fadeBoxOut");
+    assetsBox.classList.add("hidden");
     setTimeout(()=>{
-        assetsBox.classList.add("hidden");
-        assetsBox.classList.remove("fadeBoxOut");
+        watchListBox.classList.remove("fadeBoxIn");
     }, 500);
 });
 // Toggle to assets view
@@ -567,11 +577,9 @@ viewAssets.addEventListener("click", ()=>{
     viewWatchlist.classList.add("fadeColorOut");
     assetsBox.classList.remove("hidden");
     assetsBox.classList.add("fadeBoxIn");
-    watchListBox.classList.add("fadeBoxOut");
-    setTimeout(()=>{
-        watchListBox.classList.add("hidden");
-        watchListBox.classList.remove("fadeBoxOut");
-    });
+    //   watchListBox.classList.add("fadeBoxOut");
+    watchListBox.classList.add("hidden");
+//   watchListBox.classList.remove("fadeBoxOut");
 });
 // Open watchlist modal
 watchListButton.addEventListener("click", ()=>{
@@ -593,28 +601,57 @@ watchListClose.addEventListener("click", ()=>{
 });
 // Open search modal
 addItemToWatchlist.addEventListener("click", ()=>{
-    modalDropShadow.classList.remove("hidden");
     addItemDiv.classList.remove("hidden");
+    modalDropShadow.classList.remove("hidden");
+    searchCoinInput.focus();
 });
 // Close search modal
 modalDropShadow.addEventListener("click", ()=>{
-    modalDropShadow.classList.add("hidden");
     addItemDiv.classList.add("hidden");
+    modalDropShadow.classList.add("hidden");
 });
 closeSearchModal.addEventListener("click", ()=>{
-    modalDropShadow.classList.add("hidden");
     addItemDiv.classList.add("hidden");
+    modalDropShadow.classList.add("hidden");
 });
-searchInputButton.addEventListener("click", ()=>{}); // const sendData = fetch(
- //   `https://qdash-3fe95-default-rtdb.europe-west1.firebasedatabase.app/${getStorage}.json`,
- //   {
- //     method: "POST",
- //     body: JSON.stringify(someInput.value),
- //     headers: {
- //       "Content-Type": "application/json",
- //     },
- //   }
- // );
+// Used to store user inserted search value
+let userValue;
+// Search for user input (1/3)
+const coinSearcher = async ()=>{
+    let searchValue = searchCoinInput.value.toLowerCase();
+    userValue = searchValue;
+    noCoinFoundTxt.classList.add("hidden");
+    const response = await fetch(`https://api.coingecko.com/api/v3/search?query=${searchValue}`);
+    const data = await response.json();
+    if (data.coins.length === 0) {
+        noCoinFoundTxt.classList.remove("hidden");
+        return;
+    } else {
+        nameChecker(data);
+        console.log(data);
+    }
+};
+// Check for match and return right api-id (2/3)
+const nameChecker = (data)=>{
+    for (let value of Object.values(data.coins))if (value.symbol.toLowerCase() === userValue || value.name.toLowerCase() === userValue) {
+        dataFetcher(value.id);
+        return;
+    }
+};
+// Re-fetching coin and displaying result
+const dataFetcher = async (id)=>{
+    try {
+        const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}&order=market_cap_desc&per_page=100&page=1&sparkline=false`);
+        const data = await response.json();
+        const response3 = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
+        const data2 = await response3.json();
+        console.log(data[0]);
+        console.log(data2);
+    } catch (err) {
+        console.log(err);
+    }
+};
+searchInputButton.addEventListener("click", coinSearcher);
 
 },{}]},["iU07B","3X4BY"], "3X4BY", "parcelRequire379f")
 
--- scripts/watchList.js
+++ scripts/watchList.js
@@ -1,5 +1,16 @@
 "use strict";
 
+// const sendData = fetch(
+//   `https://qdash-3fe95-default-rtdb.europe-west1.firebasedatabase.app/${getStorage}.json`,
+//   {
+//     method: "POST",
+//     body: JSON.stringify(someInput.value),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   }
+// );
+
 const watchListButton = document.querySelector(".watchlist");
 const watchListClose = document.querySelector(".watch-close");
 const watchListDiv = document.querySelector(".watchlist-div");
@@ -14,6 +25,7 @@ const closeSearchModal = document.querySelector(".inner-search-close");
 const addItemDiv = document.querySelector(".add-item-div");
 const searchInputButton = document.querySelector(".add-search-button");
 const searchCoinInput = document.querySelector(".coin-search-input");
+const noCoinFoundTxt = document.querySelector(".no-search-res");
 
 // Toggle to watchlist view
 viewWatchlist.addEventListener("click", () => {
@@ -22,10 +34,10 @@ viewWatchlist.addEventListener("click", () => {
   viewAssets.classList.add("fadeColorOut");
   watchListBox.classList.remove("hidden");
   watchListBox.classList.add("fadeBoxIn");
-  assetsBox.classList.add("fadeBoxOut");
+  assetsBox.classList.add("hidden");
+
   setTimeout(() => {
-    assetsBox.classList.add("hidden");
-    assetsBox.classList.remove("fadeBoxOut");
+    watchListBox.classList.remove("fadeBoxIn");
   }, 500);
 });
 
@@ -36,11 +48,10 @@ viewAssets.addEventListener("click", () => {
   viewWatchlist.classList.add("fadeColorOut");
   assetsBox.classList.remove("hidden");
   assetsBox.classList.add("fadeBoxIn");
-  watchListBox.classList.add("fadeBoxOut");
-  setTimeout(() => {
-    watchListBox.classList.add("hidden");
-    watchListBox.classList.remove("fadeBoxOut");
-  });
+  //   watchListBox.classList.add("fadeBoxOut");
+
+  watchListBox.classList.add("hidden");
+  //   watchListBox.classList.remove("fadeBoxOut");
 });
 
 // Open watchlist modal
@@ -67,30 +78,73 @@ watchListClose.addEventListener("click", () => {
 
 // Open search modal
 addItemToWatchlist.addEventListener("click", () => {
-  modalDropShadow.classList.remove("hidden");
   addItemDiv.classList.remove("hidden");
+  modalDropShadow.classList.remove("hidden");
+  searchCoinInput.focus();
 });
 
 // Close search modal
 modalDropShadow.addEventListener("click", () => {
-  modalDropShadow.classList.add("hidden");
   addItemDiv.classList.add("hidden");
+  modalDropShadow.classList.add("hidden");
 });
 
 closeSearchModal.addEventListener("click", () => {
-  modalDropShadow.classList.add("hidden");
   addItemDiv.classList.add("hidden");
+  modalDropShadow.classList.add("hidden");
 });
 
-searchInputButton.addEventListener("click", () => {});
+// Used to store user inserted search value
+let userValue;
 
-// const sendData = fetch(
-//   `https://qdash-3fe95-default-rtdb.europe-west1.firebasedatabase.app/${getStorage}.json`,
-//   {
-//     method: "POST",
-//     body: JSON.stringify(someInput.value),
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   }
-// );
+// Search for user input (1/3)
+const coinSearcher = async () => {
+  let searchValue = searchCoinInput.value.toLowerCase();
+  userValue = searchValue;
+  noCoinFoundTxt.classList.add("hidden");
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/search?query=${searchValue}`
+  );
+  const data = await response.json();
+  if (data.coins.length === 0) {
+    noCoinFoundTxt.classList.remove("hidden");
+    return;
+  } else {
+    nameChecker(data);
+    console.log(data);
+  }
+};
+
+// Check for match and return right api-id (2/3)
+const nameChecker = (data) => {
+  for (let value of Object.values(data.coins)) {
+    if (
+      value.symbol.toLowerCase() === userValue ||
+      value.name.toLowerCase() === userValue
+    ) {
+      dataFetcher(value.id);
+      return;
+    }
+  }
+};
+
+// Re-fetching coin and displaying result
+const dataFetcher = async (id) => {
+  try {
+    const response = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+    );
+    const data = await response.json();
+
+    const response3 = await fetch(
+      `https://api.coingecko.com/api/v3/coins/${id}`
+    );
+    const data2 = await response3.json();
+    console.log(data[0]);
+    console.log(data2);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+searchInputButton.addEventListener("click", coinSearcher);
