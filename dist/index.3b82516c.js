1b961dbfa61a90f3992736f938beb8fb27f5d640
0xromrom <roman0811@live.nl>
2022-10-19 17:12:59 -0700

    top toggle working

--- dist/index.3b82516c.js
+++ dist/index.3b82516c.js
@@ -537,6 +537,14 @@ const converter = document.querySelector(".converter");
 const converterDiv = document.querySelector(".converter-div");
 const dropShadow = document.querySelector(".dropshadow-conv");
 const converterClose = document.querySelector(".conv-close");
+const converterInnerBox = document.querySelector(".conv-input-box");
+const topConvertButton = document.querySelector(".conv-top-btn");
+const botConvertButton = document.querySelector(".conv-bot-btn");
+const topInput = document.querySelector(".conv-top");
+const botInput = document.querySelector(".conv-bot");
+const topSearchInput = document.querySelector(".search-top");
+const topResultBox = document.querySelector(".top-input-conv");
+const topSearchBox = document.querySelector(".top-search-conv");
 converter.addEventListener("click", ()=>{
     dropShadow.classList.remove("hidden");
     converterDiv.classList.remove("hidden");
@@ -548,6 +556,18 @@ dropShadow.addEventListener("click", ()=>{
 converterClose.addEventListener("click", ()=>{
     dropShadow.classList.add("hidden");
     converterDiv.classList.add("hidden");
+});
+topConvertButton.addEventListener("click", ()=>{
+    topResultBox.classList.add("hidden");
+    topSearchBox.classList.remove("hidden");
+    topSearchInput.focus();
+});
+converterInnerBox.addEventListener("click", (e)=>{
+    if (e.target.classList[0] === "conv-input-box") {
+        console.log(e.target.classList[0]);
+        topResultBox.classList.remove("hidden");
+        topSearchBox.classList.add("hidden");
+    }
 });
 
 },{}]},["22jdy","jYW2n"], "jYW2n", "parcelRequire379f")
--- scripts/converter.js
+++ scripts/converter.js
@@ -4,6 +4,14 @@ const converter = document.querySelector(".converter");
 const converterDiv = document.querySelector(".converter-div");
 const dropShadow = document.querySelector(".dropshadow-conv");
 const converterClose = document.querySelector(".conv-close");
+const converterInnerBox = document.querySelector(".conv-input-box");
+const topConvertButton = document.querySelector(".conv-top-btn");
+const botConvertButton = document.querySelector(".conv-bot-btn");
+const topInput = document.querySelector(".conv-top");
+const botInput = document.querySelector(".conv-bot");
+const topSearchInput = document.querySelector(".search-top");
+const topResultBox = document.querySelector(".top-input-conv");
+const topSearchBox = document.querySelector(".top-search-conv");
 
 converter.addEventListener("click", () => {
   dropShadow.classList.remove("hidden");
@@ -18,4 +26,18 @@ dropShadow.addEventListener("click", () => {
 converterClose.addEventListener("click", () => {
   dropShadow.classList.add("hidden");
   converterDiv.classList.add("hidden");
+});
+
+topConvertButton.addEventListener("click", () => {
+  topResultBox.classList.add("hidden");
+  topSearchBox.classList.remove("hidden");
+  topSearchInput.focus();
+});
+
+converterInnerBox.addEventListener("click", (e) => {
+  if (e.target.classList[0] === "conv-input-box") {
+    console.log(e.target.classList[0]);
+    topResultBox.classList.remove("hidden");
+    topSearchBox.classList.add("hidden");
+  }
 });
